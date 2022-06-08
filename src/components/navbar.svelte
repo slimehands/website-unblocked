b2eed8c3aa8dd16df8d85d55e2752b947c1bcce2
vmdoi <rodriguesorlen@gmail.com>
2022-06-08 22:02:14 +0000

    another change nothing much

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,14 +1,19 @@
 <script lang="ts" >
 
+import { browser } from '$app/env';
+
+
+var global : any = global || window;
 function toggle() {
 	window.document.body.classList.toggle('dark')
 }
 
 // figure out why this doesnt work
 /*
-$:{ if (window.matchMedia('(prefers-color-scheme: dark)').matches) { 
-		window.document.body.classList.add('dark')
-	}
+if(browser){
+	if (window.matchMedia('(prefers-color-scheme: dark)').matches) { 
+			window.document.body.classList.add('dark')
+		}
 	else{
 		window.document.body.classList.remove('dark')
 	}
@@ -17,6 +22,9 @@ $:{ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 
 
 	
+
+
+
 
 </script>
 <button on:click={toggle}>dark mode?</button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,3 @@
-<h1>This website will become a hub for fun games, cool projects and useful <code>code</code> snippets</h1>
-<p>but right now it is nothing but a cool hub</p>
-<a href="games">games</a>
+<h1>This website will become a hub for cool projects and useful <code>code</code> snippets</h1>
+<p>but right now it is nothing but a webpage</p>
 <a href="projects">projects and snippets</a>
