122280b176b00db18013824c9c1353810885d83b
vmdoi <rodriguesorlen@gmail.com>
2022-06-13 13:18:30 +0000

    joj

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,23 +1,6 @@
 <script lang="ts" >
-
-
-
-
-
 function toggle() {
 	window.document.body.classList.toggle('dark')
 }
-
-// figure out why this doesnt work
-
-
-
-
-
-	
-
-
-
-
 </script>
 <button on:click={toggle}>dark mode</button>
